a6b2b646b2b39bd695d9135f8367f4877f2bea8f
pritammukherjee02 <you@example.com>
2022-02-10 18:04:28 +0530

    Changed the article header image location in Mob

--- components/MainContentComponents/ArticleCard.tsx
+++ components/MainContentComponents/ArticleCard.tsx
@@ -11,8 +11,8 @@ function ArticleCard({ title, description, author, date }:Props) {
   return (
     <div className='flex p-5 my-2 flex-col justify-around border border-0 border-x-2'>
         <h2 className='text-3xl cursor-pointer'>{title}</h2>
-        <div className='flex gap-3 mt-4 items-center'>
-            <div className='w-24 h-24 md:w-28 md:h-28 lg:h-32 lg:w-32 border border-blue-500 border-1 cursor-pointer rounded-lg'></div>
+        <div className='flex flex-col lg:flex-row gap-3 mt-4 items-center'>
+            <div className='w-32 h-32 lg:h-32 lg:w-32 border border-blue-500 border-1 cursor-pointer rounded-lg'></div>
             <div className='flex flex-col h-full w-8/12 p-3 px-5'>
                 <p className='cursor-pointer'>{description}</p>
                 <div className='flex gap-1 items-center mt-2'>
