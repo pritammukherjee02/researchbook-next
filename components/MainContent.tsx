1fd4e2899e3535f4a229934fd393c8bb7690f5eb
pritammukherjee02 <you@example.com>
2022-02-10 19:06:43 +0530

    Removed the side blank spaces for mobile view

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -12,7 +12,7 @@ function MainContent() {
                 <p className='mx-4 text-lg text-gray-500'>Create something new</p>
             </div>
 
-            <div className='m-3 lg:mx-5 flex flex-col'>
+            <div className='lg:m-3 lg:mx-5 flex flex-col'>
                 <ArticleCard title="A systematic approach to running a business" description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure recusandae iusto error itaque quis quasi nesciunt architecto voluptas tenetur expedita.' author='D Maxwell' date='24 Feb, 22'/>
                 <ArticleCard title='Why INTJs are the biggest assholes of all time' description='They just are' author='Clara Maxfield' date='10 Feb, 22' />
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className='h-screen'>
         <Header />
 
-        <div className='px-5 mt-5 max-w-7xl mx-auto flex flex-col lg:flex-row justify-between relative'>
+        <div className='lg:px-5 mt-5 max-w-7xl mx-auto flex flex-col lg:flex-row justify-between relative'>
           <div className='w-full lg:w-2/12'><UserInformation /></div>
           <div className='my-0 w-full lg:w-7/12'><MainContent /></div>
           <div className='hidden lg:w-3/12 lg:inline-flex'><SideBar /></div>
