dde2e6cfae20053d7a0ef03a4ae53e16a46e9f2b
pritammukherjee02 <you@example.com>
2022-02-10 19:00:59 +0530

    Made the header pic dimensions for ipad better

--- components/MainContentComponents/ArticleCard.tsx
+++ components/MainContentComponents/ArticleCard.tsx
@@ -11,9 +11,9 @@ function ArticleCard({ title, description, author, date }:Props) {
   return (
     <div className='flex p-5 my-2 flex-col justify-around border border-x-2'>
         <h2 className='text-3xl cursor-pointer'>{title}</h2>
-        <div className='flex flex-col lg:flex-row gap-3 mt-4 lg:items-center'>
-            <div className='w-full h-44 md:h-56 lg:h-32 lg:w-32 border border-blue-500 border-1 cursor-pointer rounded-lg mx-auto'></div>
-            <div className='flex flex-col h-full w-12/12 lg:w-8/12 p-3 px-5'>
+        <div className='flex flex-col md:flex-row gap-3 mt-4 md:items-center'>
+            <div className='w-full h-44 md:h-32 md:w-4/12 lg:h-32 lg:w-32 border border-blue-500 border-1 cursor-pointer rounded-lg mx-auto'></div>
+            <div className='flex flex-col h-full md:w-8/12 w-12/12 lg:w-8/12 p-3 px-5'>
                 <p className='cursor-pointer'>{description}</p>
                 <div className='flex gap-1 items-center mt-2'>
                     <p>~</p>
