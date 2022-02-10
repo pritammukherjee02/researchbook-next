5c4305db627ccc4b0b4705352c217665a447cb1a
pritammukherjee02 <you@example.com>
2022-02-10 20:25:48 +0530

    Made the partition of articleCard better

--- components/MainContentComponents/ArticleCard.tsx
+++ components/MainContentComponents/ArticleCard.tsx
@@ -12,7 +12,7 @@ function ArticleCard({ title, description, author, date }:Props) {
     <div className='flex p-5 my-2 flex-col justify-around border border-x-2'>
         <h2 className='text-3xl cursor-pointer'>{title}</h2>
         <div className='flex flex-col md:flex-row gap-3 mt-4 md:items-center'>
-            <div className='w-full h-44 md:h-32 md:w-4/12 lg:h-32 lg:w-32 border border-blue-500 border-1 cursor-pointer rounded-lg mx-auto'></div>
+            <div className='w-full h-44 md:h-32 md:w-4/12 lg:h-32 lg:w-4/12 border border-blue-500 border-1 cursor-pointer rounded-lg mx-auto'></div>
             <div className='flex flex-col h-full md:w-8/12 w-12/12 lg:w-8/12 p-3 px-5'>
                 <p className='cursor-pointer'>{description}</p>
                 <div className='flex gap-1 items-center mt-2'>
