d92f6d66a8434cdfa6b4fe9e7ccbd4654f54c56e
pritammukherjee02 <you@example.com>
2022-03-08 15:42:42 +0530

    Attempt 2 in solving bug

--- components/CreateComponents/Editor.tsx
+++ components/CreateComponents/Editor.tsx
@@ -3,21 +3,15 @@ import React, { ReactFragment } from 'react'
 import dynamic from 'next/dynamic'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const Editor = dynamic(() => import("react-draft-wysiwyg").then(module => module.Editor), { ssr: false })
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then(module => module.Editor),
+  {
+    ssr: false,
+  }
+);
 //import { Editor } from 'react-draft-wysiwyg'
 
 function EditorComponent() {
-
-  const toggleEditMode = (event: React.FocusEvent<HTMLIFrameElement>) => {
-    //richTextField.document.designMode = 'On'
-  }
-
-  const execCmd = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-
-    const button: HTMLButtonElement = event.currentTarget;
-    //execute command using button.name
-  }
 
   return (
     <div className='flex flex-col w-full h-full lg:w-8/12'>
