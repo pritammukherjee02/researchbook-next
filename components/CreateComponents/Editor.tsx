34fff2a207771196440c5962cd08ebbe639f7609
pritammukherjee02 <you@example.com>
2022-04-05 02:41:23 +0530

    Testing js in place of tsx

--- components/CreateComponents/Editor.tsx
+++ components/CreateComponents/Editor.tsx
@@ -1,22 +1,27 @@
-import React, { ReactFragment } from 'react'
+import React, { ReactFragment, useEffect, useRef, useState } from 'react'
+
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
 
 import dynamic from 'next/dynamic'
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const Editor = dynamic(
-  () => import("react-draft-wysiwyg").then(module => module.Editor),
-  {
-    ssr: false,
-  }
-);
-//import { Editor } from 'react-draft-wysiwyg'
 
 function EditorComponent() {
 
-  return (
-    <div className='flex flex-col w-full h-full lg:w-8/12'>
+  const wrapperRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if(typeof window){
+      const editor = document.createElement('div')
+      wrapperRef.current.append(editor)
+      new Quill(editor, { theme: 'snow' })
+    }
+  }, [])
 
-      <Editor  />
+  return (
+    <div className='flex flex-col w-full h-full lg:w-8/12 bg-blue-200'>
+
+      <div id="container" ref={wrapperRef}></div>
+
     </div>
   )
 }
---
+++ components/CreateComponents/EditorQuill.tsx
@@ -0,0 +1,31 @@
+import React, { ReactFragment, useEffect, useRef, useState } from 'react'
+
+import dynamic from 'next/dynamic'
+
+const Quill = dynamic(
+	() => {
+		return import('quill');
+	},
+	{ ssr: false }
+);
+
+
+function EditorComponent() {
+
+    const [value, setValue] = useState("");
+    const quillRef = useRef();
+
+    useEffect(() => {
+        console.log(quillRef.current);
+    }, [quillRef]);
+  
+    return (
+        <div className='flex flex-col w-full h-full lg:w-8/12 bg-blue-200'>
+
+            <Quill ref={quillRef} theme="snow" value={value} onChange={setValue}></Quill>
+
+        </div>
+    )
+}
+
+export default EditorComponent
