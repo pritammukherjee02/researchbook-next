572d7471ccc0815bb28df55a456befbfc435a6a9
pritammukherjee02 <you@example.com>
2022-04-13 20:25:54 +0530

    Removed the notifications label for better looks

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,7 +51,7 @@ function Header({ home, searchProp='' }:Props) {
                 <div className='hidden lg:block'>
                     <Link href='/'>
                         <h3 className='flex flex-col items-center cursor-pointer'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={ "h-6 w-6 lg:h-7 lg:w-7 text-blue-500" } viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className={ "h-7 w-7 text-blue-500" } viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                             </svg>
                             <p className='text-sm text-blue-500'>Home</p>
@@ -59,10 +59,10 @@ function Header({ home, searchProp='' }:Props) {
                     </Link>
                 </div>
                 <h3 className='flex flex-col items-center cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 lg:h-7 lg:w-7 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
                     </svg>
-                    <p className="text-sm text-gray-700">Notifications</p>
+                    <p className="text-sm text-gray-700 hidden lg:block">Notifications</p>
                 </h3>
                 <MenuButton />
             </div>
