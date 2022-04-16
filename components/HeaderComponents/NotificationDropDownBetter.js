98d3c0315a63b60eaa79e030017c9c18df65ac09
pritammukherjee02 <you@example.com>
2022-04-16 19:54:59 +0530

    Notif panel adjustment 2

--- components/HeaderComponents/NotificationDropDownBetter.js
+++ components/HeaderComponents/NotificationDropDownBetter.js
@@ -56,7 +56,7 @@ export default function Example({ page }) {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute z-10 w-screen max-w-sm px-4 mt-3 transform -translate-x-[250px] lg:-translate-x-1/2 sm:px-0">
+              <Popover.Panel className="absolute z-10 w-screen max-w-sm px-4 mt-3 transform -translate-x-52  lg:-translate-x-1/2 sm:px-0">
                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                   <div className="relative grid gap-8 bg-white p-7">
                     {solutions.map((item) => (
