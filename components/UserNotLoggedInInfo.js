ed742ed64f589749d31ba9b0579ad46c750193c2
pritammukherjee02 <you@example.com>
2022-04-28 23:58:03 +0530

    Added new side navbar to notloggedin state

--- components/UserNotLoggedInInfo.js
+++ components/UserNotLoggedInInfo.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
 //import UserInformationCard from './UserInformationComponents/UserInformationCard';
 import UserInfoNotLoggedIn from './UserInformationComponents/UserInfoNotLoggedIn'
+import SideBarNav from './UserInformationComponents/SideBarNav';
 
 function UserInformation() {
   return (
   <div className='flex flex-col space-2 lg:h-screen lg:sticky lg:pb-20 lg:top-10 scrollbar-hide overflow-y-auto'>
       <UserInfoNotLoggedIn />
+
+      <div className='my-4 mt-6 hidden lg:block'>
+          {/*<p className='text-sm font-semibold opacity-40 cursor-default'>From following</p>
+          <div className='my-3 flex flex-col gap-2'>
+            {fromFollowingUsersMarkup}
+          </div>*/}
+
+          <SideBarNav />
+      </div>
+
+      <Link href='/create'>
+        <div className='mt-5 flex items-center px-6 py-3 rounded-full cursor-pointer justify-center bg-blue-500 hover:bg-blue-600'>
+          <p className='text-xl font-semibold text-white'>Create</p>
+        </div>
+      </Link>
   </div>
   )
 }
