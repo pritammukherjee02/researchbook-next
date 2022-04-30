94a28ac1eed0f9d9f6d64689b009b443c1c75dcd
pritammukherjee02 <you@example.com>
2022-05-01 00:56:41 +0530

    Testing a bug fix in UserInfo, handling no session

--- components/UserInformation.js
+++ components/UserInformation.js
@@ -12,7 +12,7 @@ function UserInformation(props) {
   return (
   <div className='flex flex-col space-2 lg:h-full lg:fixed scrollbar-hide overflow-y-auto'>
       {/*<UserInformationCard name='James Anderson' fieldOfExpertise='Comp sci' jobDesignation='Research Fellow' uid={1} username='@janderson11' articles={0} followers={2} following={98} />*/}
-      <UserInfoCard name={session ? session.user.name : userInfo.name} fieldOfExpertise={userInfo.fieldOfExpertise} jobDesignation={userInfo.jobDesignation} uid={userInfo.uid} username={userInfo.username} pfp={session.user.image} articles={userInfo.articles} followers={userInfo.followers} following={userInfo.following} />
+      <UserInfoCard name={session ? session.user.name : userInfo.name} fieldOfExpertise={userInfo.fieldOfExpertise} jobDesignation={userInfo.jobDesignation} uid={userInfo.uid} username={userInfo.username} pfp={session ? session.user.image : ''} articles={userInfo.articles} followers={userInfo.followers} following={userInfo.following} />
 
       <div className='my-4 mt-6 hidden lg:block'>
           {/*<p className='text-sm font-semibold opacity-40 cursor-default'>From following</p>
--- pages/readlist.js
+++ pages/readlist.js
@@ -39,7 +39,7 @@ function Readlist({ session }) {
 			</Head>
 
 			<main className='h-screen overflow-x-hidden'>
-				<Header home={false} searchProp='' />
+				<Header home={false} page='readlist' searchProp='' />
 
 				<div className='lg:px-5 mt-5 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col lg:flex-row justify-between relative'>
 					<div className='hidden lg:flex w-full lg:w-2/12'>
