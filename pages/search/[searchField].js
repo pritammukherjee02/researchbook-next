03f28713cdefc210584a8b2ff7c5212b247dd05d
pritammukherjee02 <you@example.com>
2022-05-05 14:02:23 +0530

    Implemented a really crude front-end search sys

--- pages/search/[searchField].js
+++ pages/search/[searchField].js
@@ -4,6 +4,19 @@ import Head from 'next/head'
 
 import { getSession } from 'next-auth/react'
 
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+  connectSearchBox,
+} from 'react-instantsearch-dom';
+
 //Components
 import Header from '../../components/Header'
 import ArticleCard from '../../components/MainContentComponents/ArticleCard'
@@ -12,6 +25,11 @@ import UserInformation from '../../components/UserInformation'
 import UserNotLoggedInInfo from '../../components/UserNotLoggedInInfo'
 
 //The { searchField } in the name contains whatever the user searched
+
+const searchClient = algoliasearch(
+  'YLY5A2MO21',
+  'a827d5781711d37cccbfab0539cf992a'
+);
 
 function Search({ session }) {
   const router = useRouter()
@@ -44,6 +62,14 @@ function Search({ session }) {
 
   const userInformationMarkup = session ? (<UserInformation page='search' session={session} userInfo={userInfo} />) : (<UserNotLoggedInInfo page='search' />)
 
+  const SearchBox = ({ currentRefinement, refine }) => (
+    <form noValidate action="" role="search">
+      <input autoFocus name='inPageSearchField' type='search' onChange={event => refine(event.currentTarget.value)} value={currentRefinement} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />
+    </form>
+  );
+
+  const CustomSearchBox = connectSearchBox(SearchBox);
+
   return (
     <div>
       <Head>
@@ -55,22 +81,29 @@ function Search({ session }) {
 			<Header home={false} searchProp='' />
 
 			<div className='lg:px-5 mt-5 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col lg:flex-row justify-between relative'>
-				<div className='w-full hidden lg:block lg:w-2/12'>
-					{userInformationMarkup}
-				</div>
-				<div className='my-0 w-full lg:w-10/12'>
-					<div className='lg:px-3 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col justify-around gap-1'>
-						<form onSubmit={search} className='w-11/12 lg:w-7/12 mx-auto lg:mx-0'>
-							<input name='inPageSearchField' type='text' onChange={onchangeHandler} value={searchFieldContent} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />
-						</form>
+        <InstantSearch indexName="articles" searchClient={searchClient}>
+          <Configure hitsPerPage={8} />
+          <div className='w-full hidden lg:block lg:w-2/12'>
+            {userInformationMarkup}
+          </div>
+          <div className='my-0 w-full lg:w-10/12'>
+            <div className='lg:px-3 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col justify-around gap-1'>
+              <form onSubmit={search} className='w-11/12 lg:w-7/12 mx-auto lg:mx-0'>
+                {/*<input name='inPageSearchField' type='text' onChange={onchangeHandler} value={searchFieldContent} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />*/}
+                {/*<SearchBox cssClasses={{ form: 'bg-gray-200' }} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' searchAsYouType={false} />*/}
+                <CustomSearchBox />
+              </form>
 
-						<p className='mx-7 lg:mx-16 text-sm font-light'>Searching for "{searchField}"</p>
-						<div className='w-full lg:w-8/12 lg:m-3 lg:mx-5 flex flex-col'>
-							<ArticleCard title="A systematic approach to running a business" thumbnailLink="https://images.unsplash.com/photo-1619760563678-02e23d15f69f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure recusandae iusto error itaque quis quasi nesciunt architecto voluptas tenetur expedita.' author='D Maxwell' date='24 Feb, 22'/>
-							<ArticleCard title='Why INTJs are the biggest assholes of all time' thumbnailLink="https://images.unsplash.com/photo-1619760563678-02e23d15f69f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" description='They just are' author='Clara Maxfield' date='10 Feb, 22' />
-						</div>
-					</div>
-				</div>
+              <p className='mx-7 lg:mx-16 text-sm font-light'>Searching for "{searchField}"</p>
+              <div className='w-full lg:w-8/12 lg:m-3 lg:mx-5 flex flex-col'>
+                {/*<ArticleCard title="A systematic approach to running a business" thumbnailLink="https://images.unsplash.com/photo-1619760563678-02e23d15f69f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure recusandae iusto error itaque quis quasi nesciunt architecto voluptas tenetur expedita.' author='D Maxwell' date='24 Feb, 22'/>
+                <ArticleCard title='Why INTJs are the biggest assholes of all time' thumbnailLink="https://images.unsplash.com/photo-1619760563678-02e23d15f69f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" description='They just are' author='Clara Maxfield' date='10 Feb, 22' />*/}
+                <Hits hitComponent={Hit} />
+              </div>
+            </div>
+            {/*<Pagination className='flex' />*/}
+          </div>
+        </InstantSearch>
 			</div>
 
 			<AppBar currentPage='search' />
@@ -93,6 +126,12 @@ function Search({ session }) {
   		<AppBar currentPage='search' />*/}
     </div>
   )
+}
+
+function Hit({hit}) {
+  return (
+    <ArticleCard title={hit.title} thumbnailLink={hit.thumbnailLink} description={hit.description} author={hit.author} date={hit.date} />
+  );
 }
 
 export default Search
--- pages/search/index.js
+++ pages/search/index.js
@@ -4,11 +4,30 @@ import Head from 'next/head'
 
 import { getSession } from 'next-auth/react'
 
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+  connectSearchBox,
+} from 'react-instantsearch-dom';
+
 //components
 import Header from '../../components/Header'
 import AppBar from '../../components/AppBar'
 import UserInformation from '../../components/UserInformation'
 import UserNotLoggedInInfo from '../../components/UserNotLoggedInInfo'
+import ArticleCard from '../../components/MainContentComponents/ArticleCard';
+
+const searchClient = algoliasearch(
+    'YLY5A2MO21',
+    'a827d5781711d37cccbfab0539cf992a'
+  );
 
 function Index({ session }) {
     const router = useRouter()
@@ -40,6 +59,14 @@ function Index({ session }) {
 
     const userInformationMarkup = session ? (<UserInformation page='search' session={session} userInfo={userInfo} />) : (<UserNotLoggedInInfo page='search' />)
 
+    const SearchBox = ({ currentRefinement, refine }) => (
+        <form noValidate action="" role="search">
+          <input autoFocus name='inPageSearchField' type='search' onChange={event => refine(event.currentTarget.value)} value={currentRefinement} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />
+        </form>
+      );
+    
+    const CustomSearchBox = connectSearchBox(SearchBox);
+
     return (
         <div>
             <Head>
@@ -51,18 +78,25 @@ function Index({ session }) {
                 <Header home={false} searchProp='' />
 
                 <div className='lg:px-5 mt-5 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col lg:flex-row justify-between relative'>
-                    <div className='hidden lg:flex w-full lg:w-2/12'>
-                        {userInformationMarkup}
-                    </div>
-                    <div className='my-0 w-full lg:w-10/12'>
-                        <div className='lg:px-3 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col justify-around gap-1'>
-                            <form onSubmit={search} className='w-11/12 lg:w-7/12 mx-auto lg:mx-0'>
-                                <input name='inPageSearchField' type='text' onChange={onchangeHandler} value={searchFieldContent} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />
-                            </form>
-
-                            <p className='mx-7 lg:mx-16 text-sm font-light'>What do you want to read today?</p>
+                    <InstantSearch indexName="articles" searchClient={searchClient}>
+                        <Configure hitsPerPage={8} />
+                        <div className='hidden lg:flex w-full lg:w-2/12'>
+                            {userInformationMarkup}
                         </div>
-                    </div>
+                        <div className='my-0 w-full lg:w-10/12'>
+                            <div className='lg:px-3 pb-14 lg:pb-0 max-w-7xl mx-auto flex flex-col justify-around gap-1'>
+                                <form onSubmit={e => e.preventDefault()} className='w-11/12 lg:w-7/12 mx-auto lg:mx-0'>
+                                    {/*<input name='inPageSearchField' type='text' onChange={onchangeHandler} value={searchFieldContent} className='mb-2 lg:mx-9 text-md outline-none shadow-none font-semibold opacity-90 p-3 w-full rounded-full bg-gray-200' placeholder='Search...' />*/}
+                                    <CustomSearchBox />
+                                </form>
+
+                                <p className='mx-7 lg:mx-16 text-sm font-light'>What do you want to read today?</p>
+                                <div className='w-full lg:w-8/12 lg:m-3 lg:mx-5 flex flex-col'>
+                                    <Hits hitComponent={Hit} />
+                                </div>
+                            </div>
+                        </div>
+                    </InstantSearch>
                 </div>
 
                 <AppBar currentPage='search' />
@@ -82,6 +116,12 @@ function Index({ session }) {
         </div>
     )
 }
+
+function Hit({hit}) {
+    return (
+      <ArticleCard title={hit.title} thumbnailLink={hit.thumbnailLink} description={hit.description} author={hit.author} date={hit.date} />
+    );
+  }
 
 export default Index
 
