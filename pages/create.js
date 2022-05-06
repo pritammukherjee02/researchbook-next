953a8c1382c1e1f3fc6f47053baf505e2c437d21
pritammukherjee02 <you@example.com>
2022-05-07 02:42:20 +0530

    Introduced tags and the logic for creating them

--- pages/create.js
+++ pages/create.js
@@ -29,6 +29,7 @@ function Create({ session }) {
 	const descriptionRef = useRef(null)
 	const thumbnailRef = useRef(null)
 	const filePickerRef = useRef(null)
+	const tagsRef = useRef(null)
 
 	const userInfo = {
 		uid: 1,
@@ -41,7 +42,7 @@ function Create({ session }) {
 		following: 98
 	}
 
-	async function addArticleCard(articleId, url = ''){
+	async function addArticleCard(articleId, tags, url = ''){
 		const docCardRef = await addDoc(collection(db, "articleCards"), {
 			title: titleRef.current.value,
 			description: descriptionRef.current.value,
@@ -49,7 +50,8 @@ function Create({ session }) {
 			author: session.user.name,
 			uid: session.user.email,
 			articleId: articleId,
-			thumbnailLink: thumbnailRef.current.value ? thumbnailRef.current.value : url
+			thumbnailLink: thumbnailRef.current.value ? thumbnailRef.current.value : url,
+			tags: tags
 		})
 	}
 
@@ -67,6 +69,18 @@ function Create({ session }) {
       return
     }
 
+	const tagsCollection = (tagsRef.current.value).split(',')
+	const tags = []
+	tagsCollection.map(tag => {
+		if(tag[0] == ' '){
+			tag = tag.slice(1)
+		}
+		if(tag[tag.length - 1] == ' '){
+			tag = tag.slice(0, tag.length - 1)
+		}
+		tags.push(tag)
+	})
+
     try {
 		const docRef = await addDoc(collection(db, "articles"), {
 			content: contentRef.current.value,
@@ -76,7 +90,8 @@ function Create({ session }) {
 			date: '27 Jul, 22',
 			author: session.user.name,
 			uid: session.user.email,
-			thumbnailLink: thumbnailRef.current.value
+			thumbnailLink: thumbnailRef.current.value,
+			tags: tags
 		})
 
 		if(thumbnailToArticle)
@@ -86,7 +101,7 @@ function Create({ session }) {
 				getDownloadURL(ref(storage, snapshot.ref.fullPath))
 				.then(url => {
 
-					addArticleCard(docRef.id, url)
+					addArticleCard(docRef.id, tags, url)
 					addThumbnailToArticle(docRef.id, url)
 
 					removeThumbnail()
@@ -109,7 +124,7 @@ function Create({ session }) {
 		*/
 	  	} else {
 
-			addArticleCard(docRef.id)
+			addArticleCard(docRef.id, tags)
 
 			removeThumbnail()
 			contentRef.current.value = ''
@@ -158,31 +173,46 @@ function Create({ session }) {
               <div className='lg:px-5 mt-5 mb-3 pb-14 lg:pb-0 max-w-7xl mx-auto'>
                 <div className='flex gap-4'>
 
-                  <div className='hidden lg:flex w-full lg:w-2/12'>
-                	{userInformationMarkup}
-                  </div>
-                  
-                  <form onSubmit={publishArticle} className='flex flex-col w-full h-full lg:w-8/12 pb-5'>
-                    <div className='flex justify-between items-center lg:w-11/12'>
-                      <input type="text" ref={titleRef} name='title' className='p-2 text-3xl outline-none shadow-none font-bold w-full lg:w-8/12 mb-1' placeholder='Title...' />
-                      <input type='submit' value='Publish' className='px-6 h-10 mr-3 lg:mr-0 lg:h-11 text-md bg-blue-500 hover:bg-blue-600 cursor-pointer text-white rounded-md' />
-                    </div>
-                    <input type="text" ref={subtitleRef} name='subtitle' className='p-2 outline-none shadow-none text-2xl font-light opacity-50 w-full lg:w-8/12 mb-5' placeholder='Sub-title...' />
-
-                    <textarea type="text" ref={descriptionRef} name='description' placeholder='Description...' className='text-md outline-none shadow-none resize-none rounded-xl border-2 border-gray-100 p-2 mb-1 w-full mx-auto lg:mx-0 leading-relaxed font-light flex-wrap' rows={3} />
-                    <textarea type="text" ref={contentRef} name='content' placeholder='Write your masterpiece...' className='text-xl outline-none shadow-none resize-none p-2 w-full mx-auto lg:mx-0 leading-relaxed font-light flex-wrap' rows={15} />
-					<div className='flex gap-2 justify-between h-12 lg:w-8/12 mb-5'>
-                    	<input type="text" ref={thumbnailRef} name='thumbnail' className='p-1 w-7/12 lg:w-9/12 outline-none shadow-none text-md font-light opacity-50 border-t-2' placeholder='Link to thumbnail image...' />
-						<div onClick={() => filePickerRef.current.click()} className='w-4/12 lg:w-3/12 h-full flex items-center justify-center text-sm opacity-50 rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-400'>Upload Image</div>
+					<div className='hidden lg:flex w-full lg:w-2/12'>
+						{userInformationMarkup}
 					</div>
-                    <input ref={filePickerRef} onChange={addThumbnailImageToArticle} type='file' hidden />
-					{thumbnailToArticle && (
-						<div onClick={removeThumbnail} className=' flex flex-col w-4/12 filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'>
-							<Image src={thumbnailToArticle} className="object-contain hidden cursor-pointer rounded-lg mx-auto" width={313} height={176} objectFit='cover' />
-							<p className='text-xs text-red-500 text-center'>Remove</p>
+                  
+					<form onSubmit={publishArticle} className='flex flex-col w-full h-full lg:w-8/12 pb-5'>
+						<div className='flex justify-between items-center lg:w-11/12'>
+						<input type="text" ref={titleRef} name='title' className='p-2 text-3xl outline-none shadow-none font-bold w-full lg:w-8/12 mb-1' placeholder='Title...' />
+						<input type='submit' value='Publish' className='px-6 h-10 mr-3 lg:mr-0 lg:h-11 text-md bg-blue-500 hover:bg-blue-600 cursor-pointer text-white rounded-md' />
 						</div>
-					)}
-                  </form>
+						<input type="text" ref={subtitleRef} name='subtitle' className='p-2 outline-none shadow-none text-2xl font-light opacity-50 w-full lg:w-8/12 mb-5' placeholder='Sub-title...' />
+
+						<textarea type="text" ref={descriptionRef} name='description' placeholder='Description...' className='text-md outline-none shadow-none resize-none rounded-xl border-2 border-gray-100 p-2 mb-1 w-full mx-auto lg:mx-0 leading-relaxed font-light flex-wrap' rows={3} />
+						<textarea type="text" ref={contentRef} name='content' placeholder='Write your masterpiece...' className='text-xl outline-none shadow-none resize-none p-2 w-full mx-auto lg:mx-0 leading-relaxed font-light flex-wrap' rows={15} />
+						
+
+						<div name='Thumbnail' className='border-t-2 mt-2 p-3 w-full'>
+							<p className='text-md font-semibold opacity-30 mb-4 cursor-default'>Thumbnail</p>
+							<div className='flex flex-col gap-2 justify-between lg:w-full mb-5'>
+								<input type="text" ref={thumbnailRef} name='thumbnail' className='p-2 w-11/12 mx-auto rounded-xl outline-none shadow-none text-md font-light opacity-50 border-2' placeholder='Use a link to the thumbnail...' />
+
+								<span className='text-xs cursor-default font-semibold opacity-20 mx-auto'>OR</span>
+
+								{!thumbnailToArticle && <div onClick={() => filePickerRef.current.click()} className='w-11/12 lg:w-7/12 mx-auto p-8 flex items-center justify-center text-sm opacity-50 rounded-lg cursor-pointer bg-gray-100 border-gray-500 border-2 border-dashed'>Upload Image</div>}
+							</div>
+							<input ref={filePickerRef} onChange={addThumbnailImageToArticle} type='file' hidden />
+							{thumbnailToArticle && (
+								<div onClick={removeThumbnail} className=' flex flex-col mx-auto w-4/12 filter hover:brightness-105 transition duration-150 transform cursor-pointer'>
+									<Image src={thumbnailToArticle} className="object-contain hidden cursor-pointer rounded-lg mx-auto" width={313} height={176} objectFit='cover' />
+									<p className='text-xs text-red-500 text-center'>Remove</p>
+								</div>
+							)}
+						</div>
+
+						<div name='Tags' className='border-t-2 mt-2 p-3 w-full'>
+							<p className='text-md font-semibold opacity-30 mb-4 cursor-default'>Tags</p>
+							<div className='flex flex-col gap-2 justify-between lg:w-full mb-5'>
+								<input type="text" ref={tagsRef} name='tags' className='p-2 w-11/12 mx-auto rounded-xl outline-none shadow-none text-md font-light opacity-50 border-2' placeholder="Comma (',') seperated..." />
+							</div>
+						</div>
+					</form>
 
                 </div>
               </div>
