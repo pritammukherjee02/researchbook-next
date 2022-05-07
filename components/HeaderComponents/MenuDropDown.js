22329a39e87709fe60233adfc434322606a41a99
pritammukherjee02 <you@example.com>
2022-05-08 03:24:45 +0530

    Added the link to preferences page in the navbar

--- components/HeaderComponents/MenuDropDown.js
+++ components/HeaderComponents/MenuDropDown.js
@@ -98,24 +98,26 @@ export default function Example() {
               </Menu.Item>
               <Menu.Item>
                 {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <MoveActiveIcon
-                        className="w-5 h-5 mr-2"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <MoveInactiveIcon
-                        className="w-5 h-5 mr-2"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Preferences
-                  </button>
+                  <Link href='/preferences'>
+                    <button
+                      className={`${
+                        active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <MoveActiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <MoveInactiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      )}
+                      Preferences
+                    </button>
+                  </Link>
                 )}
               </Menu.Item>
             </div>
