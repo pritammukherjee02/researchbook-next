4f79e5a45629078906c02ec73b579ba865f83472
pritammukherjee02 <you@example.com>
2022-05-09 00:38:35 +0530

    Added link to editing page in articleCard

--- components/MainContentComponents/ArticleCardMenuDropdown.js
+++ components/MainContentComponents/ArticleCardMenuDropdown.js
@@ -1,5 +1,6 @@
 import { Menu, Transition } from '@headlessui/react'
 import { Fragment, useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
 
 import DeleteBtn from './DeleteBtn'
 import DeleteButton from './DeleteButton'
@@ -82,24 +83,26 @@ export default function Example({ selfOwned, articleId, articleCardId, selfUid, 
                 <div className="px-1 py-1 ">
                 <Menu.Item>
                     {({ active }) => (
-                    <button
-                        className={`${
-                        active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                        {active ? (
-                        <EditActiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                        />
-                        ) : (
-                        <EditInactiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                        />
-                        )}
-                        Edit
-                    </button>
+                      <Link href={`/edit/${articleId}`}>
+                        <button
+                            className={`${
+                            active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                            {active ? (
+                            <EditActiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            ) : (
+                            <EditInactiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            )}
+                            Edit
+                        </button>
+                      </Link>
                     )}
                 </Menu.Item>
                 </div>
