130ecc8f9148d990226e2fc2e32e037598e36d25
pritammukherjee02 <you@example.com>
2022-05-16 01:49:36 +0530

    Added some code as base to make ring rnd thumbnail

--- components/MainContentComponents/ArticleCard.tsx
+++ components/MainContentComponents/ArticleCard.tsx
@@ -64,6 +64,9 @@ function ArticleCard({ title, description, author, date, articleId, articleCardI
         <div className='flex flex-col md:flex-row gap-3 mt-4 md:items-center'>
             <Link href={`/article/${articleId}`}>
                 {/*<div className='w-full h-44 md:h-32 md:w-4/12 lg:h-32 lg:w-4/12 border border-blue-500 border-1 cursor-pointer rounded-lg mx-auto'></div>*/}
+                {/* <div className='w-full flex items-center border-2 border-red-500 h-44 md:h-[148.27px] md:w-[30%] lg:w-[45%] rounded-lg mx-auto'>
+                    <Image src={thumbnailLink} className="object-contain w-[95%] h-[95%] rounded-lg mx-auto my-auto" width={313} height={176} objectFit='cover' />
+                </div> */}
                 <Image src={thumbnailLink} className="object-contain w-full hidden md:block h-44 md:h-32 md:w-4/12 lg:h-32 lg:w-4/12 cursor-pointer rounded-lg mx-auto" width={313} height={176} objectFit='cover' />
             </Link>
             <div className='flex flex-col h-full md:w-8/12 w-12/12 lg:w-8/12 pt-1 pb-3 px-1 lg:px-5'>
